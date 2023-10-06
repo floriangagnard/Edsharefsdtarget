0ab6b2b90531358c3772625dc0bdbef59c4c04ec
Florian GAGNARD <florian@jam.gg>
2023-10-06 14:44:25 +0200

    migrate from tkinter to customtkinter

--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,5 @@
-import tkinter as tk
+import customtkinter as tk
+from customtkinter import DISABLED, E, END, N, NE, NO, NORMAL, ON, S, TOP, W, Y
 import subprocess
 import json
 from watchdog.observers import Observer
@@ -11,10 +12,13 @@ import pyttsx3
 
 import threading
 
-#from multiprocessing import Process, freeze_support  # Importez freeze_support
+tk.set_appearance_mode("dark")  # Modes: system (default), light, dark
+tk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+#from multiprocessing import Process, freeze_support 
 voice_enabled = False
 
-from tkinter import ttk
+#from customtkinter import ttk
 
 def start_log_monitor():
     log_monitor_thread = threading.Thread(target=log_monitor.run_log_monitor)
@@ -23,7 +27,7 @@ def start_log_monitor():
 
 last_wingman1_fsdtarget_value = None
 
-## synthèse vocale
+## synthse vocale
 engine = pyttsx3.init()
 text_to_speak = "New suggested FSD Target acquired : "
 ##
@@ -37,40 +41,39 @@ class JSONFileHandler(FileSystemEventHandler):
 
 
 def open_config_window():
-    config_window = tk.Toplevel(main_window)
+    config_window = tk.CTkToplevel(main_window)
     config_window.title("Configuration")
 
     with open("config_and_data.json", "r") as json_file:
         config_data = json.load(json_file)
 
-    label_your_cmd_name = ttk.Label(config_window, text="Your CMD NAME:")
+    label_your_cmd_name = tk.CTkLabel(config_window, text="Your CMD NAME:")
     label_your_cmd_name.grid(row=0, column=0, padx=10, pady=5, sticky="w")
 
-    entry_your_cmd_name = ttk.Entry(config_window, width=30)
+    entry_your_cmd_name = tk.CTkEntry(config_window, width=30)
     entry_your_cmd_name.grid(row=0, column=1, padx=10, pady=5)
     entry_your_cmd_name.insert(0, config_data["my_commander"]["name"])
 
-    label_your_apikey = ttk.Label(config_window, text="Your apikey :")
+    label_your_apikey = tk.CTkLabel(config_window, text="Your apikey :")
     label_your_apikey.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
-    entry_your_apikey = ttk.Entry(config_window, width=30)
+    entry_your_apikey = tk.CTkEntry(config_window, width=30)
     entry_your_apikey.grid(row=1, column=1, padx=10, pady=5)
     entry_your_apikey.insert(0, config_data["my_commander"]["api_key"])
 
-    label_wingman1_name = ttk.Label(config_window, text="Wingman 1 CMD NAME:")
+    label_wingman1_name = tk.CTkLabel(config_window, text="Wingman 1 CMD NAME:")
     label_wingman1_name.grid(row=2, column=0, padx=10, pady=5, sticky="w")
 
-    entry_wingman1_name = ttk.Entry(config_window, width=30)
+    entry_wingman1_name = tk.CTkEntry(config_window, width=30)
     entry_wingman1_name.grid(row=2, column=1, padx=10, pady=5)
     entry_wingman1_name.insert(0, config_data["wingman1"]["name"])
 
-    label_wingman1_apikey = ttk.Label(config_window, text="Wingman 1 apikey:")
+    label_wingman1_apikey = tk.CTkLabel(config_window, text="Wingman 1 apikey:")
     label_wingman1_apikey.grid(row=3, column=0, padx=10, pady=5, sticky="w")
 
-    entry_wingman1_apikey = ttk.Entry(config_window, width=30)
+    entry_wingman1_apikey = tk.CTkEntry(config_window, width=30)
     entry_wingman1_apikey.grid(row=3, column=1, padx=10, pady=5)
     entry_wingman1_apikey.insert(0, config_data["wingman1"]["api_key"])
-
 
     def save_config_data():
         config_data["my_commander"]["name"] = entry_your_cmd_name.get()
@@ -83,9 +86,9 @@ def open_config_window():
         
         config_window.destroy()
 
-    # Bouton enregistrer 
-    save_button = ttk.Button(config_window, text="Enregistrer", command=save_config_data)
-    save_button.grid(row=5, column=0, columnspan=2, pady=10)
+    # Bouton enregistrer
+    save_button = tk.CTkButton(config_window, text="Enregistrer", command=save_config_data)
+    save_button.grid(row=4, column=0, columnspan=2, pady=10)
 
 
 ################## fin  new window ###################
@@ -106,21 +109,21 @@ def update_labels():
     ]
 
     for label, value in zip(labels, labels_data):
-        label.config(text=f"{value[0]}{value[1]}")
+        label.configure(text=f"{value[0]}{value[1]}")
 
     main_window.after(1000, update_labels)
 
-main_window = tk.Tk()
-main_window.geometry("400x330")
+main_window = tk.CTk()
+main_window.geometry("660x400")
 main_window.title("Wingman's FSDtarget detector")
 
-police = (24)
-config_button = tk.Button(main_window, text="⚙️", command=open_config_window)
+police = ('Verdana',20)
+config_button = tk.CTkButton(main_window, text="⚙️", command=open_config_window)
 config_button.pack(side=tk.TOP)
 
 labels = []
 for _ in range(8):
-    label = tk.Label(main_window, font=police)
+    label = tk.CTkLabel(main_window, font=police)
     label.pack()
     labels.append(label)
 
@@ -135,7 +138,7 @@ def copy_wingman1_FSDTarget(wingman1_FSDTarget, autocopy=False):
         pyperclip.copy(wingman1_FSDTarget)
         if autocopy:
             pyperclip.copy(wingman1_FSDTarget)
-            if voice_toggle_state.get():  # Vérifiez si la synthèse vocale est activée
+            if voice_toggle_state.get():  
                 engine.say(text_to_speak + wingman1_FSDTarget)
                 engine.runAndWait()
 
@@ -157,39 +160,39 @@ def get_wingman1_data():
             copy_wingman1_FSDTarget(wingman1_FSDTarget, True)
             last_wingman1_fsdtarget_value = wingman1_FSDTarget
 
-        # a modifer si update à l'api trop fréquent 
+        # a modifer si update  l'api trop fréquent 
         main_window.after(10000, get_wingman1_data)
     else:
         main_window.after_cancel(get_wingman1_data)
 
 def toggle_update():
     if toggle_state.get():
-        # a modifer si update à l'api trop fréquent 
+        # a modifer si update  l'api trop frquent 
         main_window.after(10000, get_wingman1_data)
-        copy_toggle_button.config(state=tk.NORMAL)
-        copy_button.config(state=tk.NORMAL)
+        copy_toggle_button.configure(state=tk.NORMAL)
+        copy_button.configure(state=tk.NORMAL)
     else:
         main_window.after_cancel(get_wingman1_data)
-        copy_toggle_button.config(state=tk.DISABLED) 
-        copy_button.config(state=tk.DISABLED)
+        copy_toggle_button.configure(state=tk.DISABLED) 
+        copy_button.configure(state=tk.DISABLED)
 
 
 voice_toggle_state = tk.BooleanVar(value=voice_enabled)
-voice_toggle_button = tk.Checkbutton(main_window, text="Enable voice info", variable=voice_toggle_state)
+voice_toggle_button = tk.CTkCheckBox(main_window, text="Enable voice info", variable=voice_toggle_state)
 voice_toggle_button.pack()
 
 
 toggle_state = tk.BooleanVar(value=False)
-toggle_button = tk.Checkbutton(main_window, text="Toggle update wingman's info", variable=toggle_state, command=toggle_update)
+toggle_button = tk.CTkCheckBox(main_window, text="Toggle update wingman's info", variable=toggle_state, command=toggle_update)
 toggle_button.pack()
 
 copy_toggle_state = tk.BooleanVar(value=False)
-copy_toggle_button = tk.Checkbutton(main_window, text="Autocopy fsdtarget to Clipboard if new", variable=copy_toggle_state)
-copy_toggle_button.config(state=tk.DISABLED)
+copy_toggle_button = tk.CTkCheckBox(main_window, text="Autocopy fsdtarget to Clipboard if new", variable=copy_toggle_state)
+copy_toggle_button.configure(state=tk.DISABLED)
 copy_toggle_button.pack()
 
-copy_button = tk.Button(main_window, text="Copy Wingman1 FSDTarget", command=lambda: copy_wingman1_FSDTarget(wingman1_FSDTarget, False), font=("", 15))
-copy_button.config(state=tk.DISABLED)
+copy_button = tk.CTkButton(main_window, text="Copy Wingman1 FSDTarget", command=lambda: copy_wingman1_FSDTarget(wingman1_FSDTarget, False), font=("", 15))
+copy_button.configure(state=tk.DISABLED)
 copy_button.pack()
 
 
